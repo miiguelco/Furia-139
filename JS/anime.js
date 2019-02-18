d2d3977f3e0a383619b57715c18726d9d0aeee5a
Miguel Coelho <mignownow@gmail.com>
2019-02-18 22:47:04 +0000

    Ipad responsive design
    Ipad Landscape and portrait mode responsive design. Elements working correctly. Will confirm any bug after coding on the rest of devices.

--- JS/anime.js
+++ JS/anime.js
@@ -1,7 +1,7 @@
 anime({
   targets: '#front-cover',
   height: [
-     { value: 670, duration: 1000, delay: 200, elasticity: 0, easing: 'easeInSine' },
+     { value: 1150, duration: 1000, delay: 200, elasticity: 0, easing: 'easeInSine' },
      { value: 0, duration: 200, delay: 2000, elasticity: 0, easing: 'easeOutSine' },
    ],
    zIndex: [
@@ -13,7 +13,7 @@ anime({
 anime({
   targets: '.black-cover',
   height: [
-     { value: 700, duration: 0, delay: 500, elasticity: 0, easing: 'easeInSine' },
+     { value: 1150, duration: 0, delay: 500, elasticity: 0, easing: 'easeInSine' },
      { value: 0, duration: 200, delay: 2000, elasticity: 0, easing: 'easeOutSine' },
    ],
    zIndex: [
